301c7e147152db946e39dba9c63bf2d4a748110b
N <n@n.n>
2021-04-03 16:06:38 +0700

    init nextjs

---
+++ src/views/home/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useCallback, useEffect, useState } from "react";
+
+export const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>BitkubTechChallenge</title>
+      </Head>
+      <div>Hi</div>
+    </>
+  );
+};
